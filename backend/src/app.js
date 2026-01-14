990ac21d3bf1c3817e6627afc9b2093df54bac06
offshivam87 <codewithtrapnog@gmail.com>
2026-01-14 13:57:02 +0530

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 
 const app = express()
 app.use(cors({
-  origin: ["http://localhost:5173","https://moodyplayer-ei1v.onrender.com"],  // React frontend
+  origin: ["http://localhost:5173","https://ai-moody-player.vercel.app"],  // React frontend
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }))
