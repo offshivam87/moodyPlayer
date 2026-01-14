f2855aacaddd8834536aec39d7be5842a324b2d7
offshivam87 <codewithtrapnog@gmail.com>
2026-01-14 14:02:38 +0530

    cors updated finally

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,27 @@ const router = require("./routes/route")
 const cors = require("cors")
 
 const app = express()
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://moody-player-xi.vercel.app",
+  "https://ai-moody-player.vercel.app"
+];
+
 app.use(cors({
-  origin: ["http://localhost:5173","https://ai-moody-player.vercel.app"],  // React frontend
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 }));
-app.use(express.json({ limit: "10mb" }))
-app.use('/',router)
 
-module.exports= app
+
+app.use(express.json({ limit: "10mb" }))
+app.use('/', router)
+
+module.exports = app
